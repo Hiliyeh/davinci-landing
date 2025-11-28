dcfb7b110957c391ad8441f3c10774568147cce6
Hiliyeh <hiliyeh.bashir@gmail.com>
2025-11-28 20:05:06 +0100

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ import { initDisciplineFilters } from './modules/disciplines.js';
 import { initPricingTabs } from './modules/pricing.js';
 import { initRevealOnScroll } from './modules/reveal.js';
 import { initSmoothScroll } from './modules/smoothScroll.js';
-import { initStatsCounter } from './modules/stats.js';
 import { initScheduleAccordion, initScheduleHighlight } from './modules/schedule.js';
 
 initHeaderScroll();
@@ -13,6 +12,5 @@ initDisciplineFilters();
 initPricingTabs();
 initRevealOnScroll();
 initSmoothScroll();
-initStatsCounter();
 initScheduleAccordion();
 initScheduleHighlight();
